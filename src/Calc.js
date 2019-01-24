77bb2d1968bf16bd825c177699f7ba65cead1b5d
mattperkins <github@mattperkins.com>
2019-01-24 16:55:16 +0000

    set initial cache

--- src/Calc.js
+++ src/Calc.js
@@ -1,3 +1,19 @@
-import * as React from "react";
+import React, { Suspense, Fragment } from "react"
+import Fiba from './Fiba'
 
-export default () => <h2>Calc</h2>;
+const LoadingCalc = ({ num }) => <h2>Loading fibonacci for {num}</h2>
+const foo = 10
+const bar = 42
+
+export default () => {
+  return (
+    <Fragment>
+      <Suspense fallback={<LoadingCalc num={foo} />}>
+        <Fiba num={foo} />
+      </Suspense>
+      <Suspense fallback={<LoadingCalc num={bar} />}>
+        <Fiba num={bar} />
+      </Suspense>
+    </Fragment>
+  )
+}
