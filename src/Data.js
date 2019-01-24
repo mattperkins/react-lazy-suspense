ad5f2ca7294d2f062bd7af2918e49f381ba6fa70
mattperkins <github@mattperkins.com>
2019-01-24 15:08:39 +0000

    configure fetch call

--- src/Data.js
+++ src/Data.js
@@ -1,3 +1,16 @@
 import * as React from "react";
+import useFetchSuspense from "./useFetchSuspense"
 
-export default () => <h2>Data...</h2>;
+export default () => {
+  // custom hook
+  const data = useFetchSuspense("https://api-v3.mbta.com/routes", {
+    query: { "filter[route_type]": "0,1" }
+  })
+  console.log(data)
+  return <ul>
+    {/* data.data because the API endpoint array is named data */}
+    {data.data.map(route => (
+      <li key={route.id}>{route.attributes.long_name}</li>
+    ))}
+  </ul>
+}
